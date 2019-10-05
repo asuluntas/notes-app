1f6482bfd5f9b51e9745b907a7e26015e205ec47
asuluntas <aysunsulun@gmail.com>
2019-10-05 16:15:09 -0700

    Saga aadded to notes page

--- app/containers/Notes/index.js
+++ app/containers/Notes/index.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
-
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { createStructuredSelector } from 'reselect';
+// import { useInjectReducer } from 'utils/injectReducer';
+import { useInjectSaga } from 'utils/injectSaga';
+import {
+  makeSelectNotes,
+  makeSelectLoading,
+  makeSelectError,
+} from 'containers/App/selectors';
+import NotesList from 'components/NotesList';
 import H1 from 'components/H1';
+// import { loadNotes } from '../App/actions';
+import saga from './saga';
+// import { loadNotes } from '../App/actions';
 import messages from './messages';
 // import List from './List';
 // import ListItem from './ListItem';
 // import ListItemTitle from './ListItemTitle';
 
-export default function Notes() {
+const key = 'notes';
+
+export function Notes({ loading, error, notes }) {
+  // useInjectReducer({ key, reducer });
+  useInjectSaga({ key, saga });
+
+  const notesListProps = {
+    loading,
+    error,
+    notes,
+  };
+
   return (
     <div>
       <Helmet>
@@ -21,6 +46,7 @@ export default function Notes() {
       <H1>
         <FormattedMessage {...messages.header} />
       </H1>
+      <NotesList {...notesListProps} />
       {/* <List>
         <ListItem>
           <ListItemTitle>
@@ -31,3 +57,34 @@ export default function Notes() {
     </div>
   );
 }
+
+Notes.propTypes = {
+  loading: PropTypes.bool,
+  error: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
+  notes: PropTypes.oneOfType([PropTypes.array, PropTypes.bool]),
+};
+
+const mapStateToProps = createStructuredSelector({
+  notes: makeSelectNotes(),
+  loading: makeSelectLoading(),
+  error: makeSelectError(),
+});
+
+// export function mapDispatchToProps(dispatch) {
+//   return {
+//     onSubmitForm: evt => {
+//       if (evt !== undefined && evt.preventDefault) evt.preventDefault();
+//       dispatch(loadNotes());
+//     },
+//   };
+// }
+
+const withConnect = connect(
+  mapStateToProps,
+  // mapDispatchToProps,
+);
+
+export default compose(
+  withConnect,
+  memo,
+)(Notes);
